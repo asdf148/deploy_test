713523c2ee16ef3c1551e4614055c7067706fff6
asdf <wnduf0405_@naver.com>
2021-08-08 19:47:34 +0900

    Add import to application module

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApplicationRepository } from 'src/entity/repository/application.repository';
+import { UserRepository } from 'src/entity/repository/user.repository';
+import { WritingRepository } from 'src/entity/repository/writing.repository';
 import { ApplicationController } from './application.controller';
 import { ApplicationService } from './application.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserRepository, WritingRepository, ApplicationRepository])],
   controllers: [ApplicationController],
   providers: [ApplicationService]
 })
