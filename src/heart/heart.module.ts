924d360c502100cb83a6f52efe7448037a61930a
asdf <wnduf0405_@naver.com>
2021-08-08 19:19:59 +0900

    Add import to heart module

--- src/heart/heart.module.ts
+++ src/heart/heart.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HeartRepository } from 'src/entity/repository/heart.repository';
+import { UserRepository } from 'src/entity/repository/user.repository';
+import { WritingRepository } from 'src/entity/repository/writing.repository';
 import { HeartController } from './heart.controller';
 import { HeartService } from './heart.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserRepository, WritingRepository, HeartRepository])],
   controllers: [HeartController],
   providers: [HeartService]
 })
